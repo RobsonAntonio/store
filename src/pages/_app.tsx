5309f60b423c4196e012778f08e64d2dc73cec51
Robson antonio <53065263+RobsonAntonio@users.noreply.github.com>
2022-08-24 09:28:53 -0300

    document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import store from "../store/store";
 import { getTotals } from "../store/cart/cartSlice";
 import { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   store.dispatch(getTotals());
@@ -15,6 +16,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     <SkeletonTheme baseColor="#929292" highlightColor="#575757">
       <Provider store={store}>
         <ToastContainer />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          />
+        </Head>
         <Component {...pageProps} />
       </Provider>
     </SkeletonTheme>
